e10be50a96ea279edbf389f5a1cfe86e7739a8be
AndreSait <andresaiteppeland@gmail.com>
2021-01-11 00:10:07 +0100

    committing for pc transfer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { Switch, Route, Link} from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+
+
+const HatsPage = () => {
+  return (
+    <div>
+      <Link to="/hats-page">HATS PAGE</Link>
+      <h1>HATS PAGE </h1>
+  </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route exact path="/hats-page" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
