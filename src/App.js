78e133401df2e6fd730126ea24f4114830d440d1
andresait <andresaiteppeland@gmail.com>
2021-01-14 23:08:12 +0100

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ import ShopPage from "./pages/shop/shop.component";
 
 
 
+
+const TestPage = () => (
+  <h1>TEST this you dork</h1>
+)
+
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route exact path="/wasihun" component={TestPage} />
       </Switch>
     </div>
   );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import SHOP_DATA from "./shop.data.js";
 
 import CollectionPreview from "../../components/preview-collection/collection-preview.component.jsx"
 
-class ShopPage extends React.component {
+class ShopPage extends React.Component {
     constructor(props) {
         super(props);
 
@@ -15,11 +15,12 @@ class ShopPage extends React.component {
 
     render() {
         const {collections} = this.state;
-        return (
-            <div className="shop-page">{
+        return (<div className="shop-page">
+                {
                     collections.map(({ id, ...otherCollectionProps}) => (
                         <CollectionPreview key={id} {...otherCollectionProps} />
-                ))}
+                    ))
+                }
         </div>);
     }
 }
